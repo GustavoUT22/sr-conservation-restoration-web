995465380555dc54a690525b51456891cff0de2d
GustavoUT22 <gustavougartetorres@gmail.com>
2026-06-01 22:50:04 -0500

    add: specialties section

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Hero from "./components/sections/hero/Hero";
 import About from "./components/sections/about/About";
 import Works from "./components/sections/works/Works";
+import Specialties from "./components/sections/specialties/Specialties";
+;
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <About />
-      {/* <Specialties /> */}
+      <Specialties />
       {/* <CallToAction /> */}
       <Works />
       {/* <Contact /> */}
