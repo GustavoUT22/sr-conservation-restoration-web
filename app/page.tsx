70946c5e576706d4328a56b3d71d9a4e80ba4b5b
GustavoUT22 <gustavougartetorres@gmail.com>
2026-06-01 22:25:48 -0500

    add: new structure

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,10 @@ export default function Home() {
     <div>
       <Hero />
       <About />
+      {/* <Specialties /> */}
+      {/* <CallToAction /> */}
       <Works />
+      {/* <Contact /> */}
     </div>
   );
 }
